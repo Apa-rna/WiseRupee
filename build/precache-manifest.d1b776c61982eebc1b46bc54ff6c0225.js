4e06630d1dc269d718914a3338ef0d54fef1694d
Aparna <74657132+Apa-rna@users.noreply.github.com>
2023-04-07 22:58:55 +0530

    Add files via upload

---
+++ build/precache-manifest.d1b776c61982eebc1b46bc54ff6c0225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e70f75c7f952a2bd44985f48725179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e41702434ba8e41874",
+    "url": "/static/css/main.f75194a7.chunk.css"
+  },
+  {
+    "revision": "f252d30f9bcec876bbbf",
+    "url": "/static/js/2.a65d503d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a65d503d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e41702434ba8e41874",
+    "url": "/static/js/main.10ae9398.chunk.js"
+  },
+  {
+    "revision": "7e9278031d01c1eb2cee",
+    "url": "/static/js/runtime-main.24f17132.js"
+  }
+]);
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <h1>
+     
+     <img src="https://cdn-icons-png.flaticon.com/512/1533/1533036.png" alt="Girl in a jacket" width="100" height="50"></img>
+     <font face = "Helvetica" size = "50"><u><b>Wise Rupee</b></u></font><br />
+     <p>Expense Tracker</p>
+       
+       
+
+       
+      <p>Cloud computing project by Aparna, Sowmya, Bhavana, Kirti and Mounika </p>
+       
+    </h1>
+   
+  )
+}
